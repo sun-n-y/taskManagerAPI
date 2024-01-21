fb4354c6ca5a5da41eac34e6bb0259ac48446880
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-21 11:05:05 -0500

    add delete task controller logic

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -40,8 +40,18 @@ const updateTask = (req, res) => {
   res.send('update task');
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete task');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `task:${taskID}, does not exist` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
